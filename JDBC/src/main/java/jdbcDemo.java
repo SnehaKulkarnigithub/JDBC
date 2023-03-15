7dc928fb2be6c17d8300a5d54200c85d81a6f93b
ajink <ajink@DESKTOP-247VOBQ>
2023-03-15 23:28:28 +0530

    committed code

--- JDBC/src/main/java/jdbcDemo.java
+++ JDBC/src/main/java/jdbcDemo.java
@@ -1,11 +1,50 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class jdbcDemo {
 
 	public static void main(String[] args) {
-		System.out.println("Start");
-		System.out.println("Modified code");
-		System.out.println("Stop");
+		try
+		{
+		//Step 1: load the driver
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		//Step 2: Connect with database using url , username and password
+		
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/advjavajdbc_01", "root", "root");
+		
+		//Step 3: create Statement
+		
+		Statement stmt=con.createStatement();
+		
+		//Step 4 : Execute query
+		
+		ResultSet rs=stmt.executeQuery("select * form emp");
+		
+		//Step 5 : Extract data
+		
+		while (rs.next())
+		{
+			System.out.println(rs.getInt(1)+" "+rs.getString(2)+ " " +rs.getString(3));
+		}
+		con.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+			
+		}
 
 	}
 
 }
+
+
+
+
+// config.properties
+//key=value
+//rs.getInt  rs.getString
+//
